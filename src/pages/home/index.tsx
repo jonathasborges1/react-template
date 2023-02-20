ce9ccf73c755c523363ea89fa11d91f4146a71fc
Jonathas Borges <jbc@icomp.ufam.edu.br>
2023-02-20 13:25:51 -0400

    React-template - Default Standard Project Structure

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import { makeStyles } from '@mui/styles';
+import { Theme } from '@mui/material/styles';
+import { Grid } from '@mui/material';
 
 interface Props {
     children?: React.ReactNode;
 }
 
 const Home: React.FC<Props> = ({ children, ...props }) => {
+     const classes = useStyles();
     return (
-        <>
-            <h1>Este bloco representa o componente <b style={{color:"#00f"}} >home</b> </h1>
-            {children}
-        </>
+        <Grid container>
+            <Grid item>
+                <h1 className={classes.container}>Este bloco representa o componente <b style={{color:"#00f"}} >home</b> </h1>
+                {children}
+            </Grid>
+
+        </Grid>
     )
 }
 
 export default Home;
+
+const useStyles = makeStyles((theme: Theme) => ({
+    container: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  }));
