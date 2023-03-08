baf3c3eb114f3bd19830d4e0f00c3e70209ee0f7
Jonathas Borges <jbc@icomp.ufam.edu.br>
2023-03-08 16:34:15 -0400

    React-template - Default Standard Project Structure

--- src/components/routes/index.tsx
+++ src/components/routes/index.tsx
@@ -5,18 +5,18 @@ import Contact from "pages/contact";
 import Home from "pages/home";
 import Services from "pages/services";
 import React from "react";
-import { Route, Switch, HashRouter } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 const Routes: React.FC = ({ ...props}) => {
     return(
-        <HashRouter> 
+        <BrowserRouter> 
             <Switch>
                 <Route exact path={ROUTES.HOME} component={Home}/>
                 <Route path={ROUTES.ABOUT} component={About} />
                 <Route path={ROUTES.SERVICES} component={Services} />
                 <Route path={ROUTES.CONTACT} component={Contact} />
             </Switch>
-        </HashRouter>
+        </BrowserRouter>
     )
 }
 
